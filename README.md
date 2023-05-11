b567f9903fff5213b2d0210024424aa762547331
Wassup Chicken <74932143+decent-code@users.noreply.github.com>
2023-05-11 12:08:22 -0400

    etch a sketch

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+This is a html css js project for
+
+etch-a-sketch
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,42 @@
 
-const container = document.querySelector('.container');
+game();
 
-for (let i = 0; i < 32; i++) {
-    const content = document.createElement('div');
-        content.classList.add('board');
-        container.appendChild(content);
-
-        const q = document.querySelector('.board');
+function game() {
+    const userInput = prompt("Grid Numbers?");
+    console.log(userInput);
+    if (userInput === null) return;
+    if (userInput > 64 || userInput < 1)
+        {
+        alert('Invalid Input! Try between 0 - 64');
+        game();
+    } else {
+        generateBox(userInput);
+    }
 }
 
-const q = document.querySelectorAll('.board');
+function generateBox(userInput) {
+    const container = document.querySelector('.container');
 
-q.forEach(e => {
-    for (let i = 0; i < 16; i++) {
-    let content = document.createElement('div');
-    content.classList.add('box');
-    e.append(content);
-    };
-    e.addEventListener('mouseover', (c) => {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        c.target.style.background = `#${randomColor}`;
-    })
-
-});
+    for (let i = 0; i < userInput; i++) {
+        const content = document.createElement('div');
+            content.classList.add('board');
+            container.appendChild(content);
+    
+            const q = document.querySelector('.board');
+    }
+    
+    const q = document.querySelectorAll('.board');
+    
+    q.forEach(e => {
+        for (let i = 0; i < userInput; i++) {
+        let content = document.createElement('div');
+        content.classList.add('box');
+        e.append(content);
+        };
+        e.addEventListener('mouseover', (c) => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            c.target.style.background = `#${randomColor}`;
+        })
+    
+    });
+}
