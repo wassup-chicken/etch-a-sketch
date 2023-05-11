d283d503b7b4e3259e449914def6615029743df9
Wassup Chicken <74932143+decent-code@users.noreply.github.com>
2023-05-11 15:35:22 -0400

    drag and mouse over

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,7 @@
-
 game();
 
 function game() {
     const userInput = prompt("Grid Numbers?");
-    console.log(userInput);
     if (userInput === null) return;
     if (userInput > 64 || userInput < 1)
         {
@@ -14,6 +12,10 @@ function game() {
     }
 }
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 function generateBox(userInput) {
     const container = document.querySelector('.container');
 
@@ -21,22 +23,25 @@ function generateBox(userInput) {
         const content = document.createElement('div');
             content.classList.add('board');
             container.appendChild(content);
-    
-            const q = document.querySelector('.board');
     }
     
-    const q = document.querySelectorAll('.board');
+    const board = document.querySelectorAll('.board');
     
-    q.forEach(e => {
+    board.forEach(e => {
         for (let i = 0; i < userInput; i++) {
         let content = document.createElement('div');
         content.classList.add('box');
         e.append(content);
         };
-        e.addEventListener('mouseover', (c) => {
-            const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            c.target.style.background = `#${randomColor}`;
-        })
+
+        e.addEventListener('mousedown',colorIt)
+        e.addEventListener('mouseover', colorIt)
     
     });
+}
+
+function colorIt(c) {
+    if (c.type === 'mouseover' & !mouseDown) return;  
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    c.target.style.background = `#${randomColor}`;
 }
